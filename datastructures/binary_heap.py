48228a02bd2523d06914a57b2fbb7303c5ee8419
Max Kimambo <max@kimambo.de>
2018-03-05 22:51:50 +0100

    implemented heap.pop algorithm

--- datastructures/binary_heap.py
+++ datastructures/binary_heap.py
@@ -33,9 +33,6 @@ class BinaryHeap:
             return
 
         print("percolating..")
-        # find the parent
-        # current_index = self.heap.index(current_node)
-
         # Using integer division would be the same as (x -1)/2 which allows us to find the parent node
         parent_index = (current_node // 2)
 
@@ -54,11 +51,64 @@ class BinaryHeap:
             print(f"swapped {current_node} to position of {self.heap[parent_index]}")
             return self.__percolate_up(current_node)
 
+    def __percolate_down(self, node_to_percolate=0):
+
+        print("Percolating down")
+        current_node = node_to_percolate
+
+        print(f"current node {current_node}")
+
+        if not current_node:
+            last_node = self.heap.pop()
+            # Move last node to be the first
+            self.heap = [last_node] + self.heap
+
+        max_child, child_index = self.__max_child(current_node)
+
+        print(f"child {max_child} , child index: {child_index}")
+        if not max_child or not child_index:
+            return
+
+        # check if swap is necessary
+        if max_child >= self.heap[current_node]:
+            # do the swap
+            tmp = self.heap[current_node]
+            self.heap[current_node] = self.heap[child_index]
+            self.heap[child_index] = tmp
+            # swap complete
+            print(f"swapped {self.heap[child_index]} to position of {self.heap[current_node]}")
+
+            # continue percolating at the child level
+            return self.__percolate_down(child_index)
+
+    def __max_child(self, node_index):
+
+        left_child_index = (2 * node_index) + 1
+        right_child_index = (2 * node_index) + 2
+
+        try:
+            left_child = self.heap[left_child_index]
+            right_child = self.heap[right_child_index]
+
+            max_child = max(left_child, right_child)
+            if left_child == max_child:
+                return max_child, left_child_index
+
+            if right_child == max_child:
+                return max_child, right_child_index
+
+
+        except IndexError:
+            # means no children for a node exist
+            return None, None
+
     def print_heap(self):
         print(f"{self.heap}")
 
     def pop(self):
-        # TODO percolation
-        result = self.heap.pop(0)
-        return result
 
+        result = self.heap.pop(0)
+        self.__percolate_down()
+
+
+        return result
--- test/test_heap.py
+++ test/test_heap.py
@@ -1,13 +1,10 @@
-from unittest import TestCase
 import pytest
 from datastructures.binary_heap import BinaryHeap
 
 
 class TestBinaryHeap:
 
-
     def test_create_binary_heap(self):
-
         initial_nodes = [90, 80, 30, 60, 50, 100, 70, 5, 20, 10, 40, 55, 101, 45, 5]
         heap = BinaryHeap(initial_nodes)
         heap.print_heap()
@@ -17,18 +14,28 @@ class TestBinaryHeap:
         with pytest.raises(TypeError):
             heap = BinaryHeap()
 
-
-
     def test_insert_node(self):
         initial_nodes = [90, 80, 30, 60, 50, 100, 70, 5, 20, 10, 40, 55, 101, 45, 5]
         heap = BinaryHeap(initial_nodes)
         heap.push(102)
-        # heap.print_heap()
+        max_value = heap.pop()
+        assert max_value == 102
 
     def test_remove_node(self):
         initial_nodes = [90, 80, 30, 60, 50, 100, 70, 5, 20, 10, 40, 55, 101, 45, 5]
         heap = BinaryHeap(initial_nodes)
-        result = heap.pop()
+
         heap.print_heap()
+        result = heap.pop()
+
+        heap.print_heap()
+
         assert result == 101
 
+    def test_is_heap_balanced(self):
+        initial_nodes = [3383, 1851, 4975, 435, 5008, 480, 5151, 6462, 2936, 1621]
+        heap = BinaryHeap(initial_nodes)
+        max_value = heap.pop()
+        nodes_max = max(initial_nodes)
+
+        assert nodes_max == max_value
